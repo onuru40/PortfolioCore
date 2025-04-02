4aae163a6946b50fc68ca1dfb15dffe5a3783f03
onuru40 <onur.unlu40@gmail.com>
2025-04-02 22:04:32 +0300

    Partial View Geliştirmeleri

---
+++ PortfolioCore/Controllers/AdminLayoutController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioCore.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+    }
+}
