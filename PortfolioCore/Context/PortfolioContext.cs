8b6d0a871a411876b3d9c6f93a3ffb4ec4d6d862
onur_unlu40 <onur_unlu40@hotmail.com>
2025-04-08 22:05:35 +0300

    Context çalışması

---
+++ PortfolioCore/Context/PortfolioContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioCore.Entities;
+namespace PortfolioCore.Context
+{
+    public class PortfolioContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ONUR\\SQLEXPRESS;initial Catalog=PortfolioNightDb;integrated security=true;");
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
